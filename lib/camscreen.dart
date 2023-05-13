a8841ba7aa6ffd58597d3a122cd2a5e649678db3
Jingkay Simbran <angelicagrace.simbran@wvsu.edu.ph>
2023-05-13 20:41:22 +0800

    edit the design

--- lib/camscreen.dart
+++ lib/camscreen.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:deficam/dashboardScreen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:path/path.dart';
 import 'package:tflite/tflite.dart';
 import 'package:image_picker/image_picker.dart';
@@ -153,10 +155,201 @@ class _HomeState extends State<Home> {
     print('Image and prediction saved to Firebase');
   }
 
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     initializeFirebase();
     return Scaffold(
+      key: _scaffoldKey,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 20,
+                  child: Container(
+                    color: Colors.white,
+                    width: MediaQuery.of(context).size.width,
+                    height: 100.0,
+                    child: Center(
+                      child: Text(
+                        "Classify",
+                        style: GoogleFonts.roboto(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0xFF88BF3B),
+                            Color(0xFF178F3E),
+                          ]),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40),
+                          topLeft: Radius.circular(40)),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 5, top: 10),
+                              child: IconButton(
+                                color: Colors.white,
+                                icon: Icon(Icons.arrow_back_ios),
+                                iconSize: 20,
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (BuildContext) {
+                                    return dashboardScreen();
+                                  }));
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              GestureDetector(
+                                onTap: pickImage,
+                                child: Container(
+                                    width:
+                                        MediaQuery.of(context).size.width - 200,
+                                    alignment: Alignment.center,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 24, vertical: 17),
+                                    decoration: BoxDecoration(
+                                      color: Colors.lightBlueAccent,
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    child: Text(
+                                      'Capture using camera',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 16),
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              GestureDetector(
+                                onTap: pickGalleryImage,
+                                child: Container(
+                                    width:
+                                        MediaQuery.of(context).size.width - 200,
+                                    alignment: Alignment.center,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 24, vertical: 17),
+                                    decoration: BoxDecoration(
+                                      color: Colors.lightBlueAccent,
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    child: Text(
+                                      'Capture using gallery picker',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 16),
+                                    )),
+                              ),
+                              SizedBox(
+                                height: 50,
+                              ),
+                              ElevatedButton(
+                                onPressed: _saveImageAndPrediction,
+                                child: Text('Save image and prediction'),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              ElevatedButton(
+                                onPressed: _saveToFirebase,
+                                child: Text('Save image and prediction online'),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 60.0,
+                  left: 20.0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(35),
+                          border: Border.all(color: Colors.white, width: 2.0),
+                          color: Colors.white),
+                      child: Image.asset(
+                        'assets/logo/logo.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: _loading == true
+                      ? null
+                      : Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                height: MediaQuery.of(context).size.width * 0.5,
+                                width: MediaQuery.of(context).size.width * 0.5,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(30),
+                                  child: Image.file(
+                                    _image,
+                                    fit: BoxFit.fill,
+                                  ),
+                                ),
+                              ),
+                              _output.isNotEmpty
+                                  ? Text(
+                                      'The plant is suffering from a lack of ${_output[0]['label']}',
+                                    )
+                                  : Container(),
+                              Divider(
+                                height: 25,
+                                thickness: 1,
+                              ),
+                            ],
+                          ),
+                        ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+/* 
+
+return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         centerTitle: true,
@@ -267,5 +460,5 @@ class _HomeState extends State<Home> {
         ),
       ),
     );
-  }
-}
+
+*/
