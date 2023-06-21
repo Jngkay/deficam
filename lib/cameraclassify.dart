2fd395a333bff5bf6eb7fb89a0e23f71bd7ef161
Jingkay Simbran <angelicagrace.simbran@wvsu.edu.ph>
2023-06-21 14:45:03 +0800

    i have added the recomenndation & retake function

--- lib/cameraclassify.dart
+++ lib/cameraclassify.dart
@@ -1,12 +1,16 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:deficam/dashboardScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:tflite/tflite.dart';
+import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:intl/intl.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -20,6 +24,9 @@ class _CameraScreenState extends State<CameraScreen> {
   String? prediction = '';
   Timer? timer;
   bool isCapturing = false;
+  double? confidence;
+  File? capturedImage;
+  DateTime? captureTime;
 
   @override
   void initState() {
@@ -40,7 +47,7 @@ class _CameraScreenState extends State<CameraScreen> {
       isCameraReady = true;
     });
 
-    timer = Timer(Duration(seconds: 5), classifyStillImage);
+    timer = Timer(Duration(seconds: 3), classifyStillImage);
   }
 
   Future<void> loadModel() async {
@@ -52,9 +59,10 @@ class _CameraScreenState extends State<CameraScreen> {
 
   void classifyStillImage() async {
     if (cameraController!.value.isTakingPicture) return;
-    setState(() {
-      isCapturing = true; // Set capturing flag to true during image capture
-    });
+    if (timer?.isActive == false)
+      setState(() {
+        isCapturing = true; // Set capturing flag to true during image capture
+      });
     try {
       XFile imageFile = await cameraController!.takePicture();
       classifyImage(File(imageFile.path));
@@ -66,7 +74,7 @@ class _CameraScreenState extends State<CameraScreen> {
     });
     if (mounted) {
       // Delay for 3 seconds before the next classification
-      timer = Timer(Duration(seconds: 100), classifyStillImage);
+      timer = Timer(Duration(seconds: 5), classifyStillImage);
     }
   }
 
@@ -77,14 +85,169 @@ class _CameraScreenState extends State<CameraScreen> {
       threshold: 0.5, // Confidence threshold for classification
     );
 
+    if (recognitions != null && recognitions.isNotEmpty) {
+      for (var item in recognitions) {
+        final className = item['label'] as String;
+        final recommendation = await getRecommendationForClass(className);
+        item['recommendation'] = recommendation;
+      }
+    }
+
     setState(() {
-      prediction = recognitions != null ? recognitions[0]['label'] : 'Unknown';
+      if (recognitions != null && recognitions.isNotEmpty) {
+        prediction = recognitions[0]['label'];
+        confidence = recognitions[0]['confidence'];
+        capturedImage = imageFile;
+        captureTime = DateTime.now();
+      } else {
+        prediction = 'Unknown';
+        confidence = null;
+        capturedImage = null;
+        captureTime = null;
+      }
     });
+  }
+
+  Future<String> getRecommendationForClass(String className) async {
+    final jsonContent =
+        await rootBundle.loadString('assets/recommendations.json');
+    final recommendations = jsonDecode(jsonContent) as Map<String, dynamic>;
+
+    if (recommendations.containsKey(className)) {
+      final recommendation = recommendations[className] as String;
+      return recommendation;
+    } else {
+      throw Exception('Recommendation not found for class: $className');
+    }
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Text(
+                'Classification Result',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.roboto(
+                    color: Colors.black,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              if (capturedImage != null)
+                Image.file(
+                  capturedImage!,
+                  width: 150,
+                  height: 150,
+                ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                thickness: 1,
+                height: 1,
+                color: Colors.grey[350],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              if (prediction != null)
+                Text(
+                  'Deficiency Classification: ',
+                  style: GoogleFonts.roboto(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.normal),
+                ),
+              Text(
+                '$prediction',
+                style: GoogleFonts.roboto(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              if (confidence != null)
+                Text(
+                  'Confidence:',
+                  style: GoogleFonts.roboto(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.normal),
+                ),
+              Text(
+                '${confidence!.toStringAsFixed(2)}',
+                style: GoogleFonts.roboto(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                'Foliar Fertilizer Recommendation: ',
+                style: GoogleFonts.roboto(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              if (prediction != null)
+                FutureBuilder<String>(
+                  future: getRecommendationForClass(prediction!),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(
+                        snapshot.data!,
+                        style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text(
+                        'Error: ${snapshot.error}',
+                        style: TextStyle(color: Colors.red),
+                      );
+                    }
+                    return CircularProgressIndicator();
+                  },
+                ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Save result'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Close the dialog
+                  timer?.cancel();
+                  classifyStillImage();
+                },
+                child: Text('Retake'),
+              ),
+            ]),
+          ),
+        );
+      },
+    );
   }
 
   @override
   void dispose() {
-    cameraController?.dispose();
+    cameraController?.pausePreview();
     timer?.cancel();
     Tflite.close();
     super.dispose();
@@ -149,7 +312,7 @@ class _CameraScreenState extends State<CameraScreen> {
                                 icon: Icon(Icons.arrow_back_ios),
                                 iconSize: 20,
                                 onPressed: () {
-                                  cameraController?.dispose();
+                                  cameraController?.pausePreview();
                                   Navigator.push(context, MaterialPageRoute(
                                       builder: (BuildContext) {
                                     return dashboardScreen();
@@ -206,6 +369,12 @@ class _CameraScreenState extends State<CameraScreen> {
                             ],
                           ),
                         ),
+                        TextButton(
+                            onPressed: (() {
+                              timer?.cancel();
+                              _showDialog();
+                            }),
+                            child: Text('View Result'))
                       ],
                     ),
                   ),
