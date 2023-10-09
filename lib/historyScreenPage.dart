657017fe5046e10791d17ab482dab1dbb8bfb64e
Jingkay Simbran <angelicagrace.simbran@wvsu.edu.ph>
2023-10-09 20:54:50 +0800

    add date and time

--- lib/historyScreenPage.dart
+++ lib/historyScreenPage.dart
@@ -1,5 +1,5 @@
+//This is the working history screen
 import 'dart:io';
-
 import 'package:deficam/dashboardScreen.dart';
 import 'package:deficam/dbHelper.dart';
 import 'package:flutter/material.dart';
@@ -39,7 +39,9 @@ class _historyPageState extends State<historyPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    @override
+    GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+    return /*Scaffold(
       body: FutureBuilder<List<Map<String, dynamic>>>(
         future: _data,
         builder: (context, snapshot) {
@@ -84,6 +86,102 @@ class _historyPageState extends State<historyPage> {
             );
           }
         },
+      ),
+    );
+
+*/
+        Scaffold(
+      key: _scaffoldKey,
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 20,
+                  child: Container(
+                    color: Colors.white,
+                    width: MediaQuery.of(context).size.width,
+                    height: 100.0,
+                    child: Center(
+                      child: Text(
+                        "History",
+                        style: GoogleFonts.roboto(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0xFF88BF3B),
+                            Color(0xFF178F3E),
+                          ]),
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40),
+                          topLeft: Radius.circular(40)),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 5, top: 10),
+                              child: IconButton(
+                                color: Colors.white,
+                                icon: Icon(Icons.arrow_back_ios),
+                                iconSize: 20,
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (BuildContext) {
+                                    return dashboardScreen();
+                                  }));
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+
+
+                        Padding(padding: EdgeInsets.all(20), child: ,)
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 60.0,
+                  left: 20.0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(35),
+                          border: Border.all(color: Colors.white, width: 2.0),
+                          color: Colors.white),
+                      child: Image.asset(
+                        'assets/logo/logo.png',
+                        width: 80,
+                        height: 80,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
